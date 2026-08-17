8e8f8785ceb71481687b158b1d4a0f25a0d3a8fa
arunkrishna1296 <arunkrishna.1296@gmail.com>
2026-08-17 18:15:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 
+const http = require("http");
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello from DevOps CI/CD Pipeline\n");
+});
+
+server.listen(PORT, () => {
+    console.log(`Application running on port ${PORT}`);
+});
